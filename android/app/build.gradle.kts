b35d1eab6d7cebee5ced2a278219ba81af39d6e7
Mostafa Ahmed <79056512+mostafaahmedbahr@users.noreply.github.com>
2025-11-06 14:30:25 +0200

    version: 1.5.5+5

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -35,8 +35,8 @@ android {
         applicationId = "io.flyeasy.teamsapp"
         minSdk = 24
         targetSdk = 35
-        versionCode = 2
-        versionName = "2.0.0"
+        versionCode = 3
+        versionName = "3.0.0"
         multiDexEnabled = true
     }
     signingConfigs {
